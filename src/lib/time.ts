3095a12aa1e6c321427255b5bc0efd4720cb8bea
okysky1121 <58683489+okysky1121@users.noreply.github.com>
2023-08-13 21:17:45 +0900

    refactor: 불필요한 연산 제거
    * 너무 큰 값을 입력받으면 이상한 값을 반환하지만, 이 프로젝트에서는 작은 값만을 다루므로 신경쓰지 않습니다.

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -5,9 +5,9 @@ export type Time = {
 };
 
 export const toTime = (sec: number) => {
-	const second = sec % 60,
-		minute = (sec - second) / 60,
-		hour = (minute - (minute % 60)) / 60;
-
-	return { second, minute: minute % 60, hour } as Time;
+	return {
+		hour: (sec / 3600) | 0,
+		minute: ((sec % 3600) / 60) | 0,
+		second: sec % 60
+	} as Time;
 };
